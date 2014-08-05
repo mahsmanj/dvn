5b781da3b4ecf8a421484f265f34e6e62f4b007d
Mahsman <mahsmanj@gmail.com>
2014-08-05 06:38:09 -0700

    Added warning message for when user.py does not exist for SCons

--- config.py
+++ config.py
@@ -1,4 +1,16 @@
-import user
+warning = '''
+WARNING: user.py does not exist. This is used to define machine-specific
+configuration and is probably necessary for you to create. Copy user.py.example
+to user.py and populate it with your configuration.
+'''
+
+try:
+    with open('user.py') as config_file:
+        pass
+    import user
+except IOError:
+    print(warning)
+    user = object()
 
 def get_default(name, default):
     if hasattr(user, name):
@@ -19,5 +31,3 @@ BUILD_DIR = get_default('BUILD_DIR', 'build')
 BUILD_FILE = get_default('BUILD_FILE', 'build.py')
 
 INCLUDE_PATH = get_extend('INCLUDE_PATH', ['#include'])
-
-print(INCLUDE_PATH)
