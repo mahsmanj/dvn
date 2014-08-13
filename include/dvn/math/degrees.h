64cb5dcc00abde6606d7de35cf5addc6a9f82334
Joseph Mahsman <mahsmanj@gmail.com>
2014-08-13 06:19:41 -0700

    Implemented degrees and radians for math

--- include/dvn/math/degrees.h
+++ include/dvn/math/degrees.h
@@ -1,4 +1,16 @@
-#ifndef DVN_DEGREE_H
-#define DVN_DEGREE_H
+#ifndef DVN_DEGREES_H
+#define DVN_DEGREES_H
+
+#include <dvn/qty/angle.h>
+
+namespace dvn {
+namespace math {
+
+typedef qty::degrees degrees;
+typedef qty::degreesf degreesf;
+typedef qty::degreesd degreesd;
+
+} // math
+} // dvn
 
 #endif
---
+++ include/dvn/math/radians.h
@@ -0,0 +1,16 @@
+#ifndef DVN_DEGREES_H
+#define DVN_DEGREES_H
+
+#include <dvn/qty/angle.h>
+
+namespace dvn {
+namespace math {
+
+typedef qty::radians radians;
+typedef qty::radiansf radiansf;
+typedef qty::radiansd radiansd;
+
+} // math
+} // dvn
+
+#endif
